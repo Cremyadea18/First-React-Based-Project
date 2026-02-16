051f02412d5e2cfcdec3b8ef896176215826eafa
Cremyadea18 <creamydea@gmail.com>
2026-02-16 17:03:37 -0500

    Plantilla de producto

---
+++ single-product.php
@@ -0,0 +1,12 @@
+<?php
+get_header();
+
+if ( have_posts() ) :
+    while ( have_posts() ) : the_post();
+        
+        wc_get_template_part( 'content', 'single-product' );
+    endwhile;
+endif;
+
+get_footer(); 
+?>
