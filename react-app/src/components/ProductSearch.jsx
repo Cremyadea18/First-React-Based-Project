442a3f4d0efee73a2f87e9e72a9152bb79ec37b1
Cremyadea18 <creamydea@gmail.com>
2026-02-16 17:14:11 -0500

    Search component

---
+++ react-app/src/components/ProductSearch.jsx
@@ -0,0 +1,12 @@
+import ProductSearch from './components/ProductSearch'
+
+function App() {
+  return (
+    <div className="my-react-app">
+      <h2 style={{ textAlign: 'center' }}>Buscador de Productos WooCommerce</h2>
+      <ProductSearch />
+    </div>
+  )
+}
+
+export default App
