b6c1491d097755ce9bb9ed84a7710c8c676484fc
Cremyadea18 <129882503+Cremyadea18@users.noreply.github.com>
2026-02-17 07:54:39 -0500

    Woocomerce troubleshootng

--- template-react.php
+++ template-react.php
@@ -5,19 +5,10 @@
 
 get_header(); ?>
 
-<style>
-    /* Forzamos que el contenedor tenga espacio para renderizar */
-    #root {
-        min-height: 400px;
-        width: 100%;
-        display: block !important;
-        border: 1px solid #eee; /* Temporal para debug */
-    }
-</style>
 
 <main id="primary" class="site-main"> 
     <div id="root">
-        <p style="text-align:center; padding: 50px;">Cargando aplicación de React...</p>
+       
     </div>
 </main>
 
