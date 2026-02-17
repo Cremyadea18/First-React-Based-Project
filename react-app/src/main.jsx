3fbd67e86227d965aee8451caffaccf81aaf27db
Cremyadea18 <129882503+Cremyadea18@users.noreply.github.com>
2026-02-17 07:23:41 -0500

    Modificacion del archov main.jsx

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+const rootElement = document.getElementById('root');
+
+// Verificamos que el elemento existe y que no tiene una raíz ya creada
+if (rootElement) {
+  // Esta comprobación evita el error 299
+  if (!rootElement._reactRootContainer) {
+    ReactDOM.createRoot(rootElement).render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+    )
+  }
+}
