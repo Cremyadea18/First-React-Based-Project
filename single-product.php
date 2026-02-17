0f03d0ba970835512d96a11b42b51580732272c1
Cremyadea18 <creamydea@gmail.com>
2026-02-17 13:26:16 -0500

    Commit

--- single-product.php
+++ single-product.php
@@ -1,12 +1,38 @@
 <?php
 get_header();
 
-if ( have_posts() ) :
-    while ( have_posts() ) : the_post();
-        
-        wc_get_template_part( 'content', 'single-product' );
-    endwhile;
-endif;
+while ( have_posts() ) : the_post();
+    global $product; // Cargamos el objeto del producto actual
+    ?>
 
-get_footer(); 
+    <section class="custom-product-page">
+        <div class="container-hibrido">
+            
+            <div class="product-gallery">
+                <?php echo $product->get_image('full'); ?>
+            </div>
+
+            <div class="product-details">
+                <h1 class="product-title-custom"><?php the_title(); ?></h1>
+                
+                <div class="product-price-custom">
+                    <?php echo $product->get_price_html(); ?>
+                </div>
+
+                <div class="product-description-custom">
+                    <?php the_content(); ?>
+                </div>
+
+                <div class="product-action-custom">
+                    <?php woocommerce_template_single_add_to_cart(); ?>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+    <?php
+endwhile;
+
+get_footer();
 ?>
