9978b542a06204e717fa74b4a5e34b26e75f8a8f
Cremyadea18 <creamydea@gmail.com>
2026-02-18 13:27:50 -0500

    Plantilla de cart shoping

---
+++ template-carrito-custom.php
@@ -0,0 +1,20 @@
+<?php
+
+
+get_header(); 
+?>
+
+<div class="cart-page-wrapper">
+    <div class="container">
+        <?php
+       
+        while ( have_posts() ) : the_post();
+            the_content(); 
+        endwhile;
+        ?>
+    </div>
+</div>
+
+<?php
+get_footer(); 
+?>
