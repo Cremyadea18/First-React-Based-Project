7cf0e4417809c6b380fe106a2394189fd1f0b275
Cremyadea18 <129882503+Cremyadea18@users.noreply.github.com>
2026-02-18 06:39:35 -0500

    Additional changes

--- footer.php
+++ footer.php
@@ -1,9 +1,5 @@
-<div id="root"></div>
+<div id="react-footer"></div>
 
-<footer style="text-align: center; padding: 20px; background: #333; color: white; margin-top: 40px;">
-    <p>© 2026 - Mi Entorno de Pruebas - ASTRAPERSONALTHEME</p>
-</footer>
-
-<?php wp_footer(); ?>
+    <?php wp_footer(); ?>
 </body>
 </html>
