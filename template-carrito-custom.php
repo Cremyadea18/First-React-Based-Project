7de600573396bef231d8c3f4f749af53c65eea85
Cremyadea18 <creamydea@gmail.com>
2026-02-19 12:43:02 -0500

    Related products for carrito page

--- template-carrito-custom.php
+++ template-carrito-custom.php
@@ -15,6 +15,12 @@ get_header();
         endwhile;
         ?>
     </div>
+    <div class="related-products-container" style="margin-top: 80px; border-top: 1px solid rgba(0,0,0,0.05); padding-top: 40px;">
+                <?php
+                
+                woocommerce_output_related_products();
+                ?>
+            </div>
 </div>
 
 <?php
